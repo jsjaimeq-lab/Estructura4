a606d352f73da1f23281b927a73b822309d4e517
jsjaimeq-lab <jsjaimeq@ufpso.edu.co>
2026-08-16 08:13:53 -0500

    nuevo proyecto1

---
+++ Proyecto1/Arreglo/src/main/java/com/mycompany/arreglo/Arreglo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.arreglo;
+
+/**
+ *
+ * @author juan
+ */
+public class Arreglo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
