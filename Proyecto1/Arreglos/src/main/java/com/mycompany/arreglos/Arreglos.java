83141b1d15932ed16be955fd3ed68e1f924b0846
jsjaimeq-lab <jsjaimeq@ufpso.edu.co>
2026-08-17 15:34:00 -0500

    primer proyecto

---
+++ Proyecto1/Arreglos/src/main/java/com/mycompany/arreglos/Arreglos.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.arreglos;
+
+/**
+ *
+ * @author juan
+ */
+public class Arreglos {
+
+    
+}
