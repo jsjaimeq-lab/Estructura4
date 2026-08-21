8d1b31905a664e60bab8ad6b4a878c568e9ee651
jsjaimeq-lab <jsjaimeq@ufpso.edu.co>
2026-08-21 10:58:15 -0500

    inicio proyecto Universidad

---
+++ Proyecto3/Universidad/src/main/java/com/mycompany/universidad/Universidad.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.universidad;
+
+/**
+ *
+ * @author juan
+ */
+public class Universidad {
+
+    public static void main(String[] args) {
+        
+    }
+}
